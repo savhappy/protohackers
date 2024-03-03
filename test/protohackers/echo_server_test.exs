d94ddadce17924a713923d483ffee1fb271edf82
Savannah Manning <savannahmanning@lt-tw01149.lan>
2024-03-03 12:18:24 -0700

    initial

---
+++ test/protohackers/echo_server_test.exs
@@ -0,0 +1,33 @@
+defmodule Protohackers.EchoServerTest do
+  use ExUnit.Case
+  test "echoes anything back" do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 5001, mode: :binary, active: false)
+      assert :gen_tcp.send(socket, "foo") == :ok
+      assert :gen_tcp.send(socket, "bar") == :ok
+      :gen_tcp.shutdown(socket, :write)
+      assert :gen_tcp.recv(socket, 0, 5000) == {:ok, "foobar"}
+  end
+
+  test "echo server has max buffer size" do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 5001, mode: :binary, active: false)
+
+    assert :gen_tcp.send(socket, :binary.copy("a", 1024 *100 +1))
+    assert :gen_tcp.recv(socket, 0) == {:error, :closed}
+  end
+
+  test "handles multi connections" do
+    tasks =
+      for _ <- 1..4 do
+        Task.async(fn ->
+          {:ok, socket} = :gen_tcp.connect(~c"localhost", 5001, mode: :binary, active: false)
+
+          assert :gen_tcp.send(socket, "foo") == :ok
+          assert :gen_tcp.send(socket, "bar") == :ok
+          :gen_tcp.shutdown(socket, :write)
+          assert :gen_tcp.recv(socket, 0, 5000) == {:ok, "foobar"}
+        end)
+      end
+
+      Enum.each(tasks, &Task.await/1)
+  end
+end
